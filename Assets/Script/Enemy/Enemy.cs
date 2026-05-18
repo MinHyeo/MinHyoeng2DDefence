a7bd5f2a49c22767621513a57b821110c4d4e9f1
MinHyeong <minhyeong61022@gmail.com>
2026-05-18 16:56:29 +0900

    [update] 적이 데미지 받는 기능 구현
    적이 데미지 받는 기능 구현

---
+++ Assets/Script/Enemy/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private SpriteRenderer _spriteRenderer;
+
+    private EnemyData _enemyData;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        string _tempId = "enemy_01";
+        _enemyData = GameDataManager.Instance.GetEnemyData(_tempId);
+    }
+    
+    public void OnDamaged()
+    {
+        _spriteRenderer.color = Color.red;
+
+        Invoke("ChangeColor", 1f);
+    }
+
+    private void ChangeColor()
+    {
+        _spriteRenderer.color = Color.white;
+    }
+}
