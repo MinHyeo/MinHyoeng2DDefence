97f08ced51248ba250d52175c3114faf4dd3a05e
MinHyeong <minhyeong61022@gmail.com>
2026-05-21 18:35:29 +0900

    [update] 타워 드래그 앤 드랍 배치 구현
    타워를 드래그 해서 드랍하면 실제 타워 오브젝트가 배치하는 기능 구현

--- Assets/Script/Manager/TowerManager.cs
+++ Assets/Script/Manager/TowerManager.cs
@@ -12,23 +12,46 @@ public class TowerManager : MonoBehaviour
 
     [SerializeField] private Tilemap _tilemap;
 
+    public void CreateTowerTest()
+    {
+        GameObject towerPrefab = Resources.Load<GameObject>("tower_01");
+        GameObject towerObject = Instantiate(towerPrefab);
+        towerObject.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
+        towerObject.transform.position = new Vector3(towerObject.transform.position.x, towerObject.transform.position.y, 0);
+    }
+
     public bool CanPlaceTower(Vector3 cellPos)
     {
-        Vector3Int vector3Int= _tilemap.WorldToCell(cellPos);
+        Vector3Int vector3Int = _tilemap.WorldToCell(cellPos);
         if (!_tilemap.HasTile(vector3Int))
+        {
+            Debug.LogWarning("타일이 없음");
             return false;
+        }
 
         TileBase clickedTile = _tilemap.GetTile(vector3Int);
         if (clickedTile.name == "backyard_4")
+        {
+            Debug.LogWarning("배치 불가능한 타일임");
             return false;
+        }
+
+        // TODO : 이거 나중에 바꿔야함(너무 성능 이슈가 있을거 같음)
+        RaycastHit2D[] hits = Physics2D.RaycastAll(cellPos, Vector2.zero);
+        foreach(var hit in hits)
+        {
+            var component = hit.collider.GetComponent<Tower>();
+            if (component != null)
+                return false;
+        }
 
         return true;
     }
 
-    public void SpawnTower(Vector3 cellPos)
+    public void SpawnTower(string towerId, Vector3 cellPos)
     {
         Vector3Int vector3Int = _tilemap.WorldToCell(cellPos);
         Vector3 spawnPos = _tilemap.GetCellCenterWorld(vector3Int);
-        GameObjectManager.Instance.CreateTowerOjbect("tower_01", spawnPos);
+        GameObjectManager.Instance.CreateTowerOjbect(towerId, spawnPos);
     }
 }
---
+++ Assets/Script/Tower/TowerBatchObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TowerBatchObject : MonoBehaviour, IDragHandler, IPointerUpHandler
+{
+    [SerializeField] private string towerId = "tower_01";
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
+        transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("배치 시도");
+        if (TowerManager.Instance.CanPlaceTower(transform.position))
+        {
+            TowerManager.Instance.SpawnTower(towerId, transform.position);
+            gameObject.SetActive(false);
+        }
+    }
+}
