a92915de1cd6c83557b766228e25f7a6d3eb1f17
MinHyeong <minhyeong61022@gmail.com>
2026-05-21 18:37:49 +0900

    [update] 카드 뽑기 및 데이터 적용 구현, 카드를 드래그 해서 타워 배치 기능 구현
    카드 뽑기 및 데이터 적용 구현, 카드를 드래그 해서 타워 배치 기능 구현

---
+++ Assets/Script/UI/CardDeckUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CardDeckUI : MonoBehaviour
+{
+    [SerializeField] private UIButton _cardDrowButton;
+    [SerializeField] private RectTransform _cardListTransform;
+
+    private Dictionary<int, GameObject> _cardList = new Dictionary<int, GameObject>();
+    private int _cardInstanceId = 0;
+
+    private void OnEnable()
+    {
+        _cardDrowButton.BindOnClickButtonEvent(DrawCard);
+    }
+
+    private void DrawCard()
+    {
+        string path = $"Prefab/UI/MainUI/CardUI";
+        GameObject loadedObj = (GameObject)Resources.Load(path);
+        GameObject gObj = Instantiate(loadedObj, _cardListTransform);
+
+        _cardList[_cardInstanceId++] = gObj;
+
+        string randomTowerId = GetRandomTowerId();
+        gObj.GetComponent<CardUI>().InitCardUI(_cardInstanceId, randomTowerId);
+
+        //float width = _cardListTransform.sizeDelta.x;
+        //float WidthSize = width / _cardList.Count;
+    }
+
+    private string GetRandomTowerId()
+    {
+        List<string> allTowerDataIds = GameDataManager.Instance.GetAllTowerIds();
+        int index = Random.Range(0, allTowerDataIds.Count);
+        return allTowerDataIds[index];
+    }
+}
---
+++ Assets/Script/UI/CardUI.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CardUI : MonoBehaviour, IDragHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Text _towerNameText;
+    [SerializeField] private Image _towerIconImage;
+
+    private int _instanceId;
+    private string _towerId;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.localScale = Vector3.one * 1.5f;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale = Vector3.one;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            GameObject towerPrefab = Resources.Load<GameObject>(_towerId);
+            GameObject towerObject = Instantiate(towerPrefab);
+            towerObject.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
+            towerObject.transform.position = new Vector3(towerObject.transform.position.x, towerObject.transform.position.y, 0);
+
+            eventData.pointerPress = towerObject;
+            eventData.pointerDrag = towerObject;
+            ExecuteEvents.Execute(towerObject, eventData, ExecuteEvents.pointerDownHandler);
+
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void InitCardUI(int instanceId, string towerId)
+    {
+        _instanceId = instanceId;
+        _towerId = towerId;
+
+        SetCardUI();
+    }
+
+    private void SetCardUI()
+    {
+        EntityData towerData = GameDataManager.Instance.GetEntityData(_towerId);
+        _towerNameText.text = towerData.Name;
+        ResourceManager.Instance.LoadSprite(towerData.IconPath, (sprite) =>
+        {
+            if (sprite == null)
+                return;
+            _towerIconImage.sprite = sprite;
+        });
+    }
+}
